8215922c2ef966f4148889b97652b8b2959d18c3
Harshit Lalwani <harshit.lalwani@research.iiit.ac.in>
2026-06-22 01:05:33 +0000

    Refactor: Strip out all RL (RELDEC) components, leaving core BP decoder

--- RELDEC/experiments/__init__.py
+++
@@ -1,4 +0,0 @@
-from .spec import EvaluationManifest, EvaluationSpec, ExperimentSpec, RunManifest
-from .config import ConfigLoader
-
-__all__ = ["ExperimentSpec", "RunManifest", "EvaluationSpec", "EvaluationManifest", "ConfigLoader"]
--- run_all.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-export PYTHONPATH=".:ldpc/src_python"
-python3 run_batch_evaluations.py
